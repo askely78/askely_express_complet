96f2f344d087098fc13bea65262a72c395cd86a4
Moulay Idriss Semlali <bookingflyers@gmail.com>
2025-06-17 19:31:10 +0200

    Fix : correction finale de la chaîne dans authenticate()

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ def check_auth(username, password):
 
 def authenticate():
     return Response(
-        '⚠️ Accès refusé. Veuillez vous authentifier.'', 401,
+        '⚠️ Accès refusé.
+Veuillez vous authentifier.', 401,
         {'WWW-Authenticate': 'Basic realm="Askely Admin"'})
 
 def requires_auth(f):
